7c5335f7e53bb1e6cc22a3a9a4ecac10bdb039f5
zuodw <836617785@qq.com>
2017-11-16 11:17:35 +0800

    Flask

---
+++ special_modules/web/project/microblog/app/__init__.py
@@ -0,0 +1,7 @@
+#encoding=utf-8
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+from app import views
---
+++ special_modules/web/project/microblog/app/forms.py
@@ -0,0 +1,7 @@
+from flask.ext.wtf import Form # 导入Form类
+from wtforms import StringField, BooleanField # 导入2个需要的字段类： TextField和BooleanField
+from wtforms.validators import DataRequired # 验证器，简单的检查相应域提交的数据是否为空
+
+class LoginForm(Form):
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
