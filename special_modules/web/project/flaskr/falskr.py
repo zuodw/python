1ae8e539eb96c3bdc097a2a458b8c81c93eda114
zuodw <836617785@qq.com>
2017-11-28 17:24:36 +0800

    scrapy

--- special_modules/web/project/flaskr/falskr.py
+++ special_modules/web/project/flaskr/falskr.py
@@ -20,14 +20,10 @@ def connect_db():
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
+            db.cursor.executescript(f.read())
         db.commit()
-
-
-
-
-
 
 
 if __name__ == '__main__':
     app.run()
+    init_db()
